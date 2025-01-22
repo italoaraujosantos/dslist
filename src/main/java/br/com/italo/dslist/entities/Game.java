9ea0b6940df2d7419e16bc1b143fa3599763368b
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2025-01-21 23:13:48 -0300

    Game seed

--- src/main/java/br/com/italo/dslist/entities/Game.java
+++ src/main/java/br/com/italo/dslist/entities/Game.java
@@ -21,22 +21,26 @@ public class Game{
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {}
 
-	public Game(Long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataforms = plataforms;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -77,11 +81,11 @@ public class Game{
 	}
 
 	public String getPlataforms() {
-		return plataforms;
+		return platforms;
 	}
 
-	public void setPlataforms(String plataforms) {
-		this.plataforms = plataforms;
+	public void setPlataforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public Double getScore() {
